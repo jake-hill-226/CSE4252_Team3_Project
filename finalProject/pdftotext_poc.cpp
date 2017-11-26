ee86447473c3450946734c37d86dbbc885b990b5
Jake Hill <boneheadjake226@gmail.com>
2017-11-25 21:05:38 -0500

    Created a bookmark indexer for pdf files 'bookMarkMapper.exe'. Also working on generating metadata for imported books

--- finalProject/pdftotext_poc.cpp
+++ finalProject/pdftotext_poc.cpp
@@ -9,8 +9,92 @@
 
 #include <stdlib.h>
 
+#include <stdexcept>
+#include <stdio.h>
+
+#include <utility>
+
 using namespace std;
 
+
+
+std::string exec(const char* cmd) {
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL)
+                result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
+}
+
+void generateMetadata(const char* pdf_path, const char* meta_path, const char* txt_path){
+  ostream f_meta(meta_path);
+  if(f_meta.fail()){
+    cout << "Failed to create metadata file" << endl;
+    return;
+  }
+  
+  fstream f_txt(txt_path);
+  if(f_txt.fail()){
+    cout << "Failed to open txt file" << endl;
+    return;
+  }
+  
+  int curr_index = 0;
+  int num_lines = 0;
+  int num_chapters = 0;
+  pair<string, int> * chapter_map;
+  
+  // Capture content and number of lines in the file
+  string buffer;
+  string txt_contents = "";
+  while(getline(f_txt, buffer)){
+    num_lines ++;
+    
+    txt_contents += buffer;
+  }
+  
+  f_txt.close();
+  
+  // Create file mapping chapters to page numbers
+  string command = "./bookMarkMapper.exe ";
+  command.append(pdf_path);
+  exec(command.c_str());
+  
+  fstream f_chapter("./temp_toc.txt");
+  if(f_chapter.fail()){
+    cout << "Failed to open table of contents file" << endl;
+    return;
+  }
+  
+  if(getline(f_chapter, buffer)){
+    num_chapters = (int)buffer;
+  }
+  
+  if(num_chapters != 0){
+    chapter_map = new pair<string,int>[num_chapters];
+    int i = 0;
+    while(getline(f_chapter, buffer) and i < num_chapters){
+      chapter_map[i].first = buffer; // Set chapter title
+      getline(f_chapter, buffer);
+      chapter_map[i].second = buffer; // Set chapter page
+    }
+    
+    // match pages with string indecies
+    string page;
+  }
+  f_chapter.close();
+  
+}
 
 int main(){
   const char* path = "../Test_PDF_Files/SweatingCandles.pdf";
@@ -29,14 +113,6 @@ int main(){
     cout << "Path chosen does not point to a .pdf file" << endl;
     return 0;
   }
-
-  // Call executable for pdftotext given the input path
-  string command = "pdftotext -nopgbrk ";
-  command.append(path);
-  
-  cout << command << endl;
-  system(command.c_str());
-  
   
   // Get the path for the .txt output of pdftotext
   s_path.replace(index, 4, ".txt");
@@ -46,6 +122,13 @@ int main(){
   }
   
   f_export_name = s_path.substr(index+1);
+
+  // Call executable for pdftotext given the input path
+  string command = "./pdf2txt -o ./importedBooks" + f_export_name;
+  command.append(path);
+  
+  system(command.c_str());
+  
   
   // Check if pdftotext successfully exported a .txt
   f_export.open(s_path.c_str());
